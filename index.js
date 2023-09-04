90790f3548a7c2d08859604781d660c6119ba04f
Horleryheancarh <32201705+Horleryheancarh@users.noreply.github.com>
2023-09-04 13:57:02 +0000

    fix all errors

--- index.js
+++ index.js
@@ -8,20 +8,20 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/hello", function(req, res) {
+  res.json({ greeting: 'hello API' });
 });
 
 app.get('/api', (req, res) => {
@@ -34,27 +34,24 @@ app.get('/api', (req, res) => {
 
 // Date endpoint
 app.get('/api/:date?', (req, res) => {
-  let date = req.params.date;
-  const iso8601Pattern = /^\d{4}-\d{2}-\d{2}[T\d{2}:\d{2}:\d{2}(\.\d{1,3})?Z]?$/;
-  const unixPattern = /^(?:\d{10}|\d{13})$/;
+  let dateString = req.params.date;
+  let date;
 
-  let unix = new Date();
-  let utc = convertTimeToRFC822(unix);
-
-  if (date) {
-    if (iso8601Pattern.test(date)) {
-      date = new Date(date);
-    } else if (unixPattern.test(date)) {
-      date = new Date(Number(date));
+  if (!dateString) {
+    date = new Date();
+  } else {
+    if (!isNaN(dateString)) {
+      date = new Date(parseInt(dateString));
     } else {
-      res.json({ error: 'Invalid Date' })
+      date = new Date(dateString);
     }
   }
 
-  unix = date.getTime();
-  utc = convertTimeToRFC822(date);
-
-  res.json({ unix, utc });
+  if (date.toString() === 'Invalid Date') {
+    res.json({ error: date.toString() });
+  } else {
+    res.json({ unix: date.getTime(), utc: date.toUTCString() });
+  }
 });
 
 // Convert Time to RFC 822
@@ -70,10 +67,10 @@ const convertTimeToRFC822 = (date) => {
   const mins = String(date.getUTCMinutes()).padStart(2, '0');
   const secs = String(date.getUTCSeconds()).padStart(2, '0');
 
-  return `${dayOfWeek} ${dayOfMonth} ${month} ${year} ${hrs}:${mins}:${secs} GMT`
+  return `${dayOfWeek}, ${dayOfMonth} ${month} ${year} ${hrs}:${mins}:${secs} GMT`
 }
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(process.env.PORT || 3000, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
